ee1495f088497853a60eae608841ac511b421335
Satoshi Tejima <te.sato1030@gmail.com>
2021-03-01 02:12:51 +0900

    add followevent

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ from linebot.exceptions import (
 )
 
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+    MessageEvent,
+    FollowEvent,
+    TextMessage,
+    TextSendMessage,
+    ImageSendMessage,
+    TemplateSendMessage,
+    ConfirmTemplate,
+    PostbackAction
 )
 
 import os
@@ -19,7 +26,7 @@ app = Flask(__name__)
 CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
 
-line_bot_api  = LineBotApi(CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 
@@ -39,10 +46,44 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    reply_token = event.reply_token
+    user_msg = event.message.text
+    user_id = event.source.user_id
+    user_name = line_bot_api.get_profile(user_id).display_name
+
     line_bot_api.reply_message(
-        event.reply_token,
-        [TextSendMessage(text=line_bot_api.get_profile(event.source.user_id).display_name),
-        ImageSendMessage(original_content_url="https://placehold.jp/150x150.png", preview_image_url="https://placehold.jp/150x150.png")]
+        reply_token,
+        [TextSendMessage(
+            text="{}さん\n{}".format(user_name, user_msg)
+            )]
+    )
+
+
+@handler.add(FollowEvent)
+def handle_follow_message(event):
+    user_id = event.source.user_id
+    reply_token = event.reply_token
+    user_name = line_bot_api.get_profile(user_id).display_name
+
+    line_bot_api.reply_message(
+        reply_token,
+        [TextSendMessage(text="こんにちは、{}さん！\n進捗管理くんをご利用いただきありがとうございます。".format(user_name)),
+            TemplateSendMessage(alt_text="現在非対応のデバイスです。iOS版またはAndroid版のLINEをご使用ください。",
+                                template=ConfirmTemplate(
+                                    text="利用を開始しますか?",
+                                    actions=[
+                                        PostbackAction(
+                                            label="Yes",
+                                            display_text="はい",
+                                            data="action=yes_first"
+                                        ),
+                                        PostbackAction(
+                                            label="No",
+                                            display_text="いいえ",
+                                            data="action=no"
+                                        )
+                                    ]
+        ))]
     )
 
 
