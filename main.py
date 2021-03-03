6c7e41bb6124d6903df44c9c0034beb2eba85941
Satoshi Tejima <te.sato1030@gmail.com>
2021-03-03 23:05:37 +0900

    bug fix

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def handle_message(event):
             _, _, initial_date, end_date, _, _ = db_editor.get_data(user_id)
             now = datetime.datetime.utcnow() + datetime.timedelta(hours=9)
             today = datetime.date(now.year, now.month, now.day)
-            index = calc_days(today, initial_date) - 1
+            index = calc_days(today, initial_date.date()) - 1
             db_editor.update(user_id, float(nums[0]), index)
             # 一緒にグラフデータも送る
             target = db_editor.get_work_target(user_id)
